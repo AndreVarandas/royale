6ac15fd1040258c5bd7dac16c7952e6b28d5ad8c
André Varandas <andre.m.varandas@gmail.com>
2017-06-12 22:41:57 +0100

    Card Detail
    Added missing card detail route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,7 @@ const ctrlCards = require('../controllers/cards');
 /* GET home page. */
 router.get('/', ctrlCards.index);
 
+/**GET card detail */
+router.get('/cards/:cardId', ctrlCards.getOne);
+
 module.exports = router;
